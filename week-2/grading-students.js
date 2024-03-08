edb9b1501c41187f57e4dc53c411a6c2082e470d
Damon Malkiewicz <46790806+dcmalk@users.noreply.github.com>
2024-03-08 11:11:07 -0500

    Add 'Grading Students` challenge

--- week-2/grading-students.js
+++ week-2/grading-students.js
@@ -1,7 +1,18 @@
-function gradingStudents(grades) {
-  // Write your code here
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
 
-  return grades;
+function gradingStudents(grades) {
+  return grades.map((grade) => {
+    if (grade < 38) {
+      return grade;
+    }
+    let nextMultiple = Math.ceil(grade / 5) * 5;
+    return nextMultiple - grade < 3 ? nextMultiple : grade;
+  });
 }
 
 function main() {
